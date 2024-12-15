53df45edf086e93c23cc6d645435a7ff572ddac3
Onur <onur.kayhan90@gmail.com>
2024-12-15 19:55:26 +0100

    Add: Add CustomUser model blueprint

--- src/main/kotlin/com/onurkayhann/__minute_stoic_be/model/CustomUser.kt
+++ src/main/kotlin/com/onurkayhann/__minute_stoic_be/model/CustomUser.kt
@@ -1,7 +1,17 @@
 package com.onurkayhann.__minute_stoic_be.model
 
-import jakarta.persistence.Entity
+import jakarta.persistence.*
 
 @Entity
-class CustomUser {
+@Table(name = "StoicUser")
+class CustomUser(
+    val username: String = "",
+    val password: String = "",
+    val isLoggedIn: Boolean = false,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null
+) {
+
 }
