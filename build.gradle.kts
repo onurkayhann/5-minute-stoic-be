405eddae5f7379a7dfd7871f93d5d6eb79518320
Onur <onur.kayhan90@gmail.com>
2024-12-16 17:48:04 +0100

    Add: Validation constraints to user credentials

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,9 @@ dependencies {
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+
+
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 }
 
 kotlin {
--- src/main/kotlin/com/onurkayhann/__minute_stoic_be/model/CustomUser.kt
+++ src/main/kotlin/com/onurkayhann/__minute_stoic_be/model/CustomUser.kt
@@ -2,11 +2,19 @@ package com.onurkayhann.__minute_stoic_be.model
 
 import com.fasterxml.jackson.annotation.JsonManagedReference
 import jakarta.persistence.*
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Size
 
 @Entity
 @Table(name = "StoicUser")
 data class CustomUser(
+
+    @NotEmpty
+    @Size(min = 2, max = 16)
     val username: String = "",
+
+    @NotEmpty
+    @Size(min = 7, max = 30)
     val password: String = "",
 
     @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true)
